293f79426aa5408b6ef23aecddf81960f23d3932
Li <36532694+ShadeEmsigima@users.noreply.github.com>
2018-10-31 12:46:30 +0100

    Sorting!
    - Made a function that can at least sort two values in a vector in decending order. (large to lil)
    
    NEXT
    Do the same for longer vectors. I belive end(), begin(), erase() and push_back() are key here.

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -15,6 +15,10 @@
 		return (a > b ? a:b);
 	}
 
+	template<typename T> T minOf(T a, T b) { // extract the smallest
+		return (a < b ? a : b);
+	}
+
 	template<typename T> T sort(T a, T b) { // 9 args
 		for( i = 0; i <  i++)
 			for (int j = 0; j < colours.size(); j++) {
@@ -23,18 +27,37 @@
 
 	//a function that can print elements of a vector type string
 	void printVectorString(std::vector<std::string> a) { //for a vector that you get
+		std::cout << "Printing vector of type string: ";
 		for (int i = 0; i < a.size(); i++) { // for every element
 			std::cout << a.at(i) << " "; //print the element
 		}
 		std::cout << std::endl;
 	}
 
-	//a function that can print elements of a vector type double
+
 	void printVectorDouble(std::vector<double> a) { //for a vector that you get
+		std::cout << "Printing vector of type double: ";
 		for (int i = 0; i < a.size(); i++) { // for every element
 			std::cout << a.at(i) << " "; //print the element
 		}
 		std::cout << std::endl;
+	}
+	//a function that can print elements of a vector type double
+	void sortVectorDouble(std::vector<double> a) { //for a vector that you get
+		
+		std::vector<double> tempVec{ a.at(0), a.at(1) }; //copy the original vector values
+		//std::cout << "Sorting vector of type double" << std::endl; //print what youll do
+		std::cout << "First 2 values are "<< tempVec.at(0)<< " " << tempVec.at(1) << std::endl;//check which variables tempvec has
+
+		//now sort the tempVec, larger to smaller value:
+		if (tempVec.at(0) < tempVec.at(1)) { //if the values are not in decending order
+			double tempVar = tempVec.at(0);//save the first to temporary variable
+			tempVec.erase(tempVec.begin());//remove the first
+			tempVec.push_back(tempVar);//add it to the back of the tempVector
+			std::cout << "The new order is: " << tempVec.at(0) << " " <<tempVec.at(1) << std::endl;
+		}
+		else { std::cout << "The first value is already greater than the second" << std::endl; }
+		
 	}
 
 int main() {
@@ -61,10 +84,20 @@ int main() {
 
 	std::vector<double> numbers{ 10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635 };
 	printVectorDouble(numbers);//print the elements of vector type double
+	sortVectorDouble(numbers);//sort the elements of vector type double
 	// gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
 	// 1) alle negatieve elementen te verwijderen
 	// 2) voor alle elementen te bepalen of ze even of oneven zijn
 	// 3) de som, het gemiddelde, en het product van alle getallen te berekenen
 
+	std::cout << std::endl;
 	system("PAUSE");
 }
+
+/*NEVER USED CODE
+			if (a.at(i) != a.at(0)) {// if youre not the first element
+				if (a.at(i) < a.at(i - 1)) {//if its smaller than the one one position less (left)
+					a.push_back(a.at(i));//plonk it at the end of the vector
+				}
+
+			}*/
