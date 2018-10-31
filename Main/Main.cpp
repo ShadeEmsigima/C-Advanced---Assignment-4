17655c4635e15504e20001d01f63397ae1c7a9c7
Li <36532694+ShadeEmsigima@users.noreply.github.com>
2018-10-31 16:25:04 +0100

    Backup before apple comparison
    - so close, damn.
    
    NEXT
    Iterators vs. insert

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -42,56 +42,95 @@
 		}
 		std::cout << std::endl;
 	}
-	//a function that can print elements of a vector type double
+	
 	void sortVectorDouble(std::vector<double> a) { //for a vector that you get
-		
+
+		std::cout << std::endl << "Initiating function sortVectorDouble ==========================" << std::endl;
 		std::vector<double> tempVec{ a.at(0), a.at(1) }; //copy the original vector values
-		//std::cout << "Sorting vector of type double" << std::endl; //print what youll do
-		std::cout << "First 2 values are "<< tempVec.at(0)<< " " << tempVec.at(1) << std::endl;//check which variables tempvec has
-
-		//now sort the tempVec, larger to smaller value:
-		if (tempVec.at(0) < tempVec.at(1)) { //if the values are not in decending order
-			double tempVar = tempVec.at(0);//save the first to temporary variable
-			tempVec.erase(tempVec.begin());//remove the first
-			tempVec.push_back(tempVar);//add it to the back of the tempVector
-			std::cout << "The new order is: " << tempVec.at(0) << " " <<tempVec.at(1) << std::endl;
+		std::cout << "Original vector looks like this: ";
+		for (int i = 0; i < 9; i++) {
+			std::cout << a.at(i) << " ";
 		}
-		else { std::cout << "The first value is already greater than the second" << std::endl; }
+		std::cout << std::endl;
+
+		std::cout << "TempVec takes first two values of the original vector: " << tempVec.at(0) << " " << tempVec.at(1) << std::endl;//check which variables tempvec has
+		//std::cout << "the second last element is: ";
+		//std::cout << tempVec.at(tempVec.size() - 2); // -2 not 1, because the actuall last element isnt an element
+		std::cout << std::endl;
+
+		//for as long as the original vector is lon
+		int orVecLeng = a.size();
+		//std::cout << "Size of original vector is: " << orVecLeng<<std::endl;
+		for (int i = 2; i < orVecLeng; i++) {
+
+			//print what the vector currently holds.
+			std::cout << "Elements in tempVec currently: ";
+			int tempVecLength = tempVec.size();
+			for (int i = 0; i < tempVecLength; i++) {
+				std::cout << tempVec.at(i) << " ";
+			}
+			std::cout << std::endl;
+			std::cout << "the second last element is: ";
+			std::cout << tempVec.at(tempVec.size() - 2);
+			std::cout << std::endl;
+
+
+			//now actually sort the tempVec, larger to smaller value:
+			std::cout << "Sorting..." << std::endl << std::endl;
+
+				if (tempVec.at(tempVec.size()-2) < tempVec.at(tempVec.size()-1)) { //if the values are not in decending order
+					double tempVar = tempVec.at(tempVec.size()-2);//save the first to temporary variable
+					tempVec.erase(tempVec.end()-2);//remove the first
+					tempVec.push_back(tempVar);//add it to the back of the tempVector
+					//std::cout << "The new order is: " << tempVec.at(0) << " " << tempVec.at(1) << std::endl;
+				}
+				else { std::cout << "The first value is already greater than the second" << std::endl; }
+
+				//now add new elements to the tempVec from the original one.
+				std::cout << "Adding next element from original vector into thempVec..." << std::endl;
+				tempVec.push_back(a.at(i));
+				//std::cout << std::endl;
+			}
+		std::cout << "Completed adding, final list: ";
+		for (int i = 0; i < tempVec.size(); i++) {
+			std::cout << tempVec.at(i) << " ";
+		}
+		std::cout << std::endl;
+		}
+
+		int main() {
+
+			std::cout << maxOf<int>(7, 9) << std::endl;
+			std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
+			printVectorString(colours);
+
+
+			// gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
+			// 1) de vector in 2 nieuwe vectoren te splitsen, 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+			// 2) alle elementen UPPERCASE te maken.
+			// 3) alle dubbele te verwijderen
+
+
+
+			/*for (std::vector<string>::const_iterator i = colours.begin(); i != colours.end(); ++i) {
+				std::cout << *i << ' ';
+			}*/
+
+			/*for (int i = 0; i < colours.size; i++) {
+				std::cout << colours;
+			}*/
+
+			std::vector<double> numbers{ 10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635 };
+			printVectorDouble(numbers);//print the elements of vector type double
+			sortVectorDouble(numbers);//sort the elements of vector type double
+			// gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
+			// 1) alle negatieve elementen te verwijderen
+			// 2) voor alle elementen te bepalen of ze even of oneven zijn
+			// 3) de som, het gemiddelde, en het product van alle getallen te berekenen
+
+			std::cout << std::endl;
+			system("PAUSE");
 		
-	}
-
-int main() {
-
-	std::cout << maxOf<int>(7, 9)<<std::endl;
-	std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
-	printVectorString(colours);
-	
-
-	// gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
-	// 1) de vector in 2 nieuwe vectoren te splitsen, 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
-	// 2) alle elementen UPPERCASE te maken.
-	// 3) alle dubbele te verwijderen
-
-	
-
-	/*for (std::vector<string>::const_iterator i = colours.begin(); i != colours.end(); ++i) {
-		std::cout << *i << ' ';
-	}*/
-
-	/*for (int i = 0; i < colours.size; i++) {
-		std::cout << colours;
-	}*/
-
-	std::vector<double> numbers{ 10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635 };
-	printVectorDouble(numbers);//print the elements of vector type double
-	sortVectorDouble(numbers);//sort the elements of vector type double
-	// gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
-	// 1) alle negatieve elementen te verwijderen
-	// 2) voor alle elementen te bepalen of ze even of oneven zijn
-	// 3) de som, het gemiddelde, en het product van alle getallen te berekenen
-
-	std::cout << std::endl;
-	system("PAUSE");
 }
 
 /*NEVER USED CODE
